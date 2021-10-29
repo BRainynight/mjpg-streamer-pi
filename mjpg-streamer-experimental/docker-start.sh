5177f4573ada01d3675fd0fde2302cccc28ebf9c
BRainynight <47938665+BRainynight@users.noreply.github.com>
2021-10-29 16:36:09 +0800

    Update docker-start.sh

--- mjpg-streamer-experimental/docker-start.sh
+++ mjpg-streamer-experimental/docker-start.sh
@@ -1,5 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 set -e
+export LD_LIBRARY_PATH="/mjpg-streamer/mjpg-streamer-experimental"
+export ENV_FPS=${ENV_FPS:-"15"}
+export ENV_RESOLUTION=${ENV_RESOLUTION:-"640x480"}
+export ENV_PORT=${ENV_PORT:-"8080"}
+export ENV_LOCATION=${ENV_LOCATION:-"/usr/local/www"}
+export ENV_CAMERA=${ENV_CAMERA:-"/dev/video0"}
 
 export LD_LIBRARY_PATH="/mjpg-streamer/mjpg-streamer-experimental"
-./mjpg_streamer -o "$1" -i "$2"
+./mjpg_streamer -i "input_uvc.so -r $ENV_RESOLUTION -d $ENV_CAMERA -f $ENV_FPS" -o "output_http.so -p $ENV_PORT -w $ENV_LOCATION"
